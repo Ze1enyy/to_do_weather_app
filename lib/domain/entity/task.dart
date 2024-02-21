7e805c8e2a10b9795b3bfee514f05d4944fd8f30
EdwardZelenyi <eduard.zeleny@tbrgroup.software>
2024-02-21 15:56:44 +0200

    Project setup

---
+++ lib/domain/entity/task.dart
@@ -0,0 +1,13 @@
+class Task {
+  Task(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.category,
+      required this.isCompleted});
+  final int id;
+  final String title;
+  final String description;
+  final String category;
+  final bool isCompleted;
+}
